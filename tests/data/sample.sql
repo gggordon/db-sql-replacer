86f00624f0d7696023e275b512c9b407a7862a70
gggordon <ggg_gilroy@yahoo.com>
2020-10-16 18:55:59 -0500

    added test data

---
+++ tests/data/sample.sql
@@ -0,0 +1,10 @@
+SELECT 1; 
+--
+--
+
+INSERT INTO cars (name,value) values ('1','2'), ('3','4');
+
+--
+--
+
+INSERT INTO bus (name,value) values ('https://trading.com','6'), ('s:19:"https://trading.com";','8');
