24644ab638953b9e6a5a242038b72a71e3848772
gggordon <ggg_gilroy@yahoo.com>
2020-10-18 17:44:01 -0500

    Added Doc Style Comments

--- src/PhpSqlReplacer/Exceptions/SqlReplacerException.php
+++ src/PhpSqlReplacer/Exceptions/SqlReplacerException.php
@@ -2,6 +2,12 @@
 
 namespace PhpSqlReplacer\Exceptions;
 
+/**
+ * SQL Replacer Exception
+ * @author gggordon
+ * @copyright gggordon 2020
+ * @license MIT
+ */
 class SqlReplacerException extends \Exception
 {
 }
--- src/PhpSqlReplacer/PhpSqlReplacer.php
+++ src/PhpSqlReplacer/PhpSqlReplacer.php
@@ -4,36 +4,59 @@ namespace PhpSqlReplacer;
 
 use PhpSqlReplacer\Exceptions\SqlReplacerException;
 
+/**
+ * PhpSqlReplacer - Main Driver Class
+ * @author gggordon
+ * @copyright gggordon 2020
+ * @license MIT
+ */
 class PhpSqlReplacer
 {
-    public function extractSqlValuesFromFile($filePath, $patterToMatchFor=null, $includeColumnNames=false)
+    /**
+     * Extracts full record values and column names from insert statements within a file
+     * For eg. if the the record contains `'I like trading'` and `trading` is the pattern being matched
+     * `'I like trading'` will be extracted.
+     *
+     * @param string $filePath - Path to source file
+     * @param string $patternToMatchFor - Optional pattern to match for. Default: null
+     * @throws Exceptions\SqlReplacerException
+     * @param bool  $includeColumnNames - Whether to include column names. Default:false
+     * @return string[]
+     */
+    public function extractSqlValuesFromFile($filePath, $patternToMatchFor=null, $includeColumnNames=false)
     {
         if (!file_exists($filePath) || !is_string($filePath)) {
             throw new SqlReplacerException("File '$filePath' does not exist");
         }
         return $this->extractSqlValues(
            file_get_contents($filePath),
-           $patterToMatchFor,
+           $patternToMatchFor,
            $includeColumnNames
        );
     }
 
-    public function extractSqlValues($contents, $patterToMatchFor=null, $includeColumnNames=false)
+    /**
+     * Extracts full record values and column names from insert statements within a string
+     * For eg. if the the record contains `'I like trading'` and `trading` is the pattern being matched
+     * `'I like trading'` will be extracted.
+     *
+     * @param string $contents - SQL Contents to search
+     * @param string $patternToMatchFor - Optional pattern to match for. Default: null
+     * @throws Exceptions\SqlReplacerException
+     * @param bool  $includeColumnNames - Whether to include column names. Default:false
+     * @return string[]
+     */
+    public function extractSqlValues($contents, $patternToMatchFor=null, $includeColumnNames=false)
     {
-        $startTime = time();
-        $currentDate=date("Y-m-d H:i:s");
-        
         $tokens=[];
         if (preg_match_all("/\(.*\)/U", $contents, $matches)) {
             $tokens=$matches[0];
         }
-        $difference = (time()-$startTime)/1000/60;
-        //echo "end token split - $difference - ".date("Y-m-d H:i:s")."\n";
+        
         $tokens = array_filter($tokens, function ($token) {
             return $token[0]=="(" && $token[strlen($token)-1]==")";
         });
         $tokens = array_map(function ($piece) use ($includeColumnNames) {
-            // var_dump($piece);
             $piece = trim(trim($piece, "("), ")");
             $pieces = explode(",", $piece);
             $pieces= array_filter(array_map('trim', $pieces), function ($p) {
@@ -41,15 +64,13 @@ class PhpSqlReplacer
             });
             $pieces = array_values($pieces);
             if (!$includeColumnNames && count($pieces)>0) {
-                // var_dump($pieces[0]);
                 $firstPiece = $pieces[0];
-                //echo $firstPiece."\n";
+                
                 
                 if ($firstPiece[0] != "'" &&
                     $firstPiece[0] != "\"" &&
                     preg_match("/[a-zA-Z]/", $firstPiece[0])
                     ) {
-                    //echo "\n$firstPiece is a column \n";
                     return [];
                 }
             }
@@ -57,9 +78,9 @@ class PhpSqlReplacer
         }, $tokens);
         
         $matches = [];
-        array_walk_recursive($tokens, function ($value) use (&$matches,$patterToMatchFor) {
-            if ($patterToMatchFor==null ||
-                       stripos($value, $patterToMatchFor) !== false) {
+        array_walk_recursive($tokens, function ($value) use (&$matches,$patternToMatchFor) {
+            if ($patternToMatchFor==null ||
+                       stripos($value, $patternToMatchFor) !== false) {
                 $matches[]=$value;
             }
         });
@@ -68,6 +89,14 @@ class PhpSqlReplacer
         return $matches;
     }
 
+    /**
+     * Safely (including managing serialized data) replaces matched string patterns in a string
+     *
+     * @param string $contents - String to search
+     * @param string $value - Value to replace
+     * @param string $replaceWith - Value to replace with
+     * @return string Updated Contents
+     */
     public function replaceValue($contents, $value, $replaceWith)
     {
         set_error_handler(function ($errno, $errstr, $errfile, $errline) {
@@ -100,6 +129,16 @@ class PhpSqlReplacer
         return $contents;
     }
 
+    /**
+     * Safely (including managing serialized data) replaces matched string patterns in a file and optionally saves to another file
+     *
+     * @param string $sourceFile - Source file to search
+     * @param string $value - Value to replace
+     * @param string $replaceWith - Value to replace with
+     * @param string $destinationFile - Destination file to write to
+     * @throws Exceptions\SqlReplacerException
+     * @return string Updated Contents
+     */
     public function replaceValueFromFile($sourceFile, $value, $replaceWith, $destinationFile=null)
     {
         if (!file_exists($sourceFile) || !is_string($sourceFile)) {
@@ -120,6 +159,15 @@ class PhpSqlReplacer
         return $contents;
     }
 
+    /**
+     * Returns a boolean value to indicate whether data is serialized
+     *
+     * Retrieved from https://developer.wordpress.org/reference/functions/is_serialized/
+     *
+     * @param string $data - Value to check to see if was serialized.
+     * @param bool $strict - Whether to be strict about the end of the string.
+     * @return bool  Whether value is serialized or not
+     */
     public function isSerializedValue($data, $strict=true)
     {
         //CREDIT TO: Wordpress.com
